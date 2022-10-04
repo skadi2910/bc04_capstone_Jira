86d3d0248a9e5fee7cb47366ad3f42c51c4b8d87
Tri Luong <viettriluong@gmail.com>
2022-10-04 08:19:10 +0700

    delete bootstrap

--- src/pages/User/Signup/Signup.jsx
+++ src/pages/User/Signup/Signup.jsx
@@ -1,7 +1,76 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Signup() {
     return (
-        <div>Signup</div>
-    )
+
+        <div className="bg-grey-lighter min-h-screen flex flex-col text">
+            <div className="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+                <div className="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <h1 className="mb-8 text-3xl text-center font-bold">Đăng ký</h1>
+                    <input
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="email"
+                        placeholder="Email"
+                    />
+
+                    <input
+                        type="text"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="name"
+                        placeholder="Tên"
+                    />
+                    <input
+                        type="password"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="passWord"
+                        placeholder="Mật khẩu"
+                    />
+                    <input
+                        type="number"
+                        className="block border border-grey-light w-full p-3 rounded mb-4"
+                        name="phoneNumber"
+                        placeholder="Số điện thoại"
+                    />
+                    <button
+                        type="submit"
+                        className="w-full text-center py-3 rounded text-white bg-blue-400 duration-200 hover:bg-blue-600 focus:outline-none my-1"
+                    >
+                        Tạo tài khoản
+                    </button>
+                    <div className="text-center text-sm text-grey-dark mt-4">
+                        Với việc đăng ký, bạn đồng ý với
+                        <a
+                            className="no-underline border-b border-grey-dark text-grey-dark"
+                            href="#"
+                        >
+                            {" "}
+                            Điều khoản dịch vụ
+                        </a>{" "}
+                        và
+                        <a
+                            className="no-underline border-b border-grey-dark text-grey-dark"
+                            href="#"
+                        >
+                            {" "}
+                            Chính sách bảo mật
+                        </a>
+                    </div>
+                </div>
+                <div className="text-grey-dark mt-6">
+                    Bạn đã có tài khoản?
+                    <NavLink
+                        className="no-underline border-b border-blue text-blue"
+                        // href="../login/"
+                        to="/login"
+                    >
+                        {" "}  Đăng nhập.
+                    </NavLink>
+
+                </div>
+            </div>
+        </div>
+
+    );
 }
--- src/templates/UserTemplate/UserTemplate.jsx
+++ src/templates/UserTemplate/UserTemplate.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import backgroundUser from "../../assets/img/bgUser.jpg"
+import LoginAnimation from '../../pages/User/Login/LoginAnimation/LoginAnimation';
+// import LoginAnimation from './LoginAnimation/LoginAnimation';
+
 export default function UserTemplate(props) {
     // ! xử lý resize window
     const [{ width, height }, setSize] = useState({
@@ -17,9 +20,19 @@ export default function UserTemplate(props) {
 
     const { Component } = props;
     return (
-        <div
-        >
-            <Component />
+        <div >
+            <section className="h-screen">
+                <div className=" px-6  h-full w-full">
+                    <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800 w-full">
+                        <div className="md:w-8/12 lg:w-6/12 mb-12 md:mb-0">
+                            <LoginAnimation />
+                        </div>
+                        <div className="md:w-8/12 lg:w-5/12 lg:ml-20 " >
+                            <Component />
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 }
